1fd209dd4de3ce82c04155a10dd3dd850e0d0202
xGabrielDEV <58668092+xGabrielDEV@users.noreply.github.com>
2020-08-09 15:34:18 -0300

    Remove main scheduler

--- external-api/src/main/java/com/engine/external/project/ExternalServer.java
+++ external-api/src/main/java/com/engine/external/project/ExternalServer.java
@@ -2,15 +2,11 @@ package com.engine.external.project;
 
 import com.engine.external.project.event.EventManager;
 import com.engine.external.project.utils.tasks.TaskManager;
-import reactor.core.scheduler.Scheduler;
-import reactor.core.scheduler.Schedulers;
 
 public abstract class ExternalServer {
 
     private EventManager eventManager;
     private TaskManager taskManager;
-
-    private final Scheduler scheduler = Schedulers.newSingle("Main Scheduler");
 
     public EventManager getEventManager() {
         return eventManager;
@@ -28,7 +24,4 @@ public abstract class ExternalServer {
         this.taskManager = taskManager;
     }
 
-    public Scheduler getScheduler() {
-        return scheduler;
-    }
 }
